a95ad8983bd02a1862a8dc3cf518e6f88668c4b8
Sam Nicholls <samstudio8@gmail.com>
2021-12-02 17:06:01 +0000

    Bump EO refresh_tokens

--- bin/pre-elan/refresh_tokens.sh
+++ bin/pre-elan/refresh_tokens.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/bash
 source ~/.bootstrap.sh
 source "$EAGLEOWL_CONF/ocarina.env"
+source "$EAGLEOWL_CONF/envs.env"
 
 eval "$(conda shell.bash hook)"
-conda activate samstudio8
+conda activate $CONDA_OCARINA
 
 ocarina --quiet --env oauth refresh
