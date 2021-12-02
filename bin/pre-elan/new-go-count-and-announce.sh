377bb71c31e0bad50243f117dbffe8b9c61e5a35
Sam Nicholls <samstudio8@gmail.com>
2021-12-02 17:04:34 +0000

    Bump EO count and annouce

--- bin/pre-elan/new-go-count-and-announce.sh
+++ bin/pre-elan/new-go-count-and-announce.sh
@@ -3,10 +3,16 @@ source ~/.bootstrap.sh
 source "$EAGLEOWL_CONF/ocarina.env"
 source "$EAGLEOWL_CONF/paths.env"
 source "$EAGLEOWL_CONF/mqtt.env"
+source "$EAGLEOWL_CONF/slack.env" # Needed for !$2 to resolve to an actual slack hook
+source "$EAGLEOWL_CONF/envs.env"
 
 # conda env and credentials
 eval "$(conda shell.bash hook)"
-conda activate samstudio8
+conda activate $CONDA_OCARINA
+
+# This script is usually run in the eagle-owl scratch dir
+# and can safely be tested anywhere other than the scratch dir
+# Passing any REAL Slack hook will have a side effect of triggering the synclist
 
 set -euo pipefail
 DATESTAMP=`date '+%Y%m%d'`
@@ -19,12 +25,12 @@ echo "[ELAN]" `date` " - LETS ROLL"
 #set -o pipefail
 
 # Keep the v2 requests for now
-#ocarina --oauth --quiet --env get sequencing --run-name '*' --faster --tsv --task-wait-attempts 75 --task-wait > latest.$1.tsv
+ocarina --oauth --quiet --env get sequencing --run-name '*' --faster --tsv --task-wait-attempts 75 --task-wait > latest.$1.tsv
 
 # Map filesystem to metadata to find work to do
 set +o pipefail
 echo "[ELAN]" `date` " - Find files"
-#find /cephfs/covid/bham/*/upload -type f -name "*fa*" | grep -v '\.fai$' | python $ELAN_SOFTWARE_DIR/bin/ocarina_resolve.py latest.$1.tsv > q.$1.tsv 2> t.$1.txt
+find /cephfs/covid/bham/*/upload -type f -name "*fa*" | grep -v '\.fai$' | python $ELAN_SOFTWARE_DIR/bin/ocarina_resolve.py latest.$1.tsv > q.$1.tsv 2> t.$1.txt
 set -o pipefail
 
 # Save manifest at the end of the day (SHORT message fires last) but only if the slack hook contains REAL
@@ -33,13 +39,8 @@ if [ "$1" = "SHORT" ] && [[ "$2" == *REAL* ]]; then
     set +o pipefail
     grep '^1' q.$1.tsv | awk '{print $8"/"$10"\n"$8"/"$12}' > $SYNCPATH
     set -o pipefail
-    python mqtt-message.py -t 'COGUK/infrastructure/housekeeping/synclisting/status' --host $MQTT_HOST --attr status finished --attr date $DATESTAMP --attr synclist $SYNCPATH
-fi
-
-if [[ "$2" == *REAL* ]]; then
-    cp t.$1.txt $COG_PUBLISHED_DIR/elan/$DATESTAMP.missing.ls
-    chmod 644 $COG_PUBLISHED_DIR/elan/$DATESTAMP.missing.ls
+    mqtt-message.py -t 'COGUK/infrastructure/housekeeping/synclisting/status' --host $MQTT_HOST --attr status finished --attr date $DATESTAMP --attr synclist $SYNCPATH
 fi
 
 ocarina --quiet --oauth --env get summary --md > summary.$1.md
-$ELAN_SOFTWARE_DIR/bin/message_uploads.sh latest.$1.tsv q.$1.tsv t.$1.txt $1 "${!2}" summary.$1.md
+$ELAN_SOFTWARE_DIR/bin/message_uploads.sh latest.$1.tsv q.$1.tsv t.$1.txt $1 "${!2}" summary.$1.md $DATESTAMP
