cf6eb979867214353123cdf014603169f6952925
Sam Nicholls <sam@samnicholls.net>
2022-01-17 19:56:42 +0000

    [wip] Simpler Elan configuration and handling
    * Load one single central config from eagleowl
    * Clarify the slack hooks
    * Remove assumption that eagleowl log dir is the elan log dir (easier testing)
    * Add NXF_WORK and NXF_CONDA_CACHEDIR as required envcars to reduce risk of stomping

--- bin/control/go-full-elan.sh
+++ bin/control/go-full-elan.sh
@@ -11,17 +11,11 @@ while read var; do
       [ -z "${!var}" ] && { echo 'Global Eagle Owl variable '$var' is empty or not set. Environment likely uninitialised. Aborting.'; exit 64; }
 done << EOF
 EAGLEOWL_CONF
-EAGLEOWL_SCRATCH
-EAGLEOWL_LOG
 EOF
 
 # Init the Elan environment from the Eagle Owl config dir
-source "$EAGLEOWL_CONF/elan.env"
-source "$EAGLEOWL_CONF/paths.env"
-source "$EAGLEOWL_CONF/envs.env"
-source "$EAGLEOWL_CONF/slack.env"
-source "$EAGLEOWL_CONF/mqtt.env"
-source "$EAGLEOWL_CONF/service_elan.env"
+source "$EAGLEOWL_CONF/elan/dev.env"
+#source "$EAGLEOWL_CONF/elan/prod.env"
 
 DATESTAMP=$1
 while read var; do
@@ -32,28 +26,33 @@ ELAN_CONFIG
 ELAN_DIR
 ELAN_SOFTWARE_DIR
 NEXTFLOW_BIN
-SLACK_MGMT_HOOK
-SLACK_REAL_HOOK
+ELAN_SLACK_MGMT_HOOK
+ELAN_SLACK_INBOUND_HOOK
 MQTT_HOST
 COG_PUBLISHED_DIR
 COG_PUBLISH_MODE
-EAGLEOWL_LOG
+ELAN_LOG_DIR
 MAJORA_DOMAIN
+NXF_WORK
+NXF_CONDA_CACHEDIR
 EOF
 
 cd $ELAN_SOFTWARE_DIR
 
+
 MSG='{"text":"*COG-UK inbound pipeline begins...*
 _HERE WE GO!_"}'
-curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
 
 echo $DATESTAMP
 
 # Centralise .nextflow.log location
-mkdir -p $EAGLEOWL_LOG/elan/$DATESTAMP
-ELAN_STEP1_NFLOG="$EAGLEOWL_LOG/elan/$DATESTAMP/nf.elan.log"
-ELAN_STEP2_NFLOG="$EAGLEOWL_LOG/elan/$DATESTAMP/nf.ocarina.log"
-ELAN_STEP3_LOG="$EAGLEOWL_LOG/elan/$DATESTAMP/publish.log"
+mkdir -p $ELAN_LOG_DIR/$DATESTAMP
+ELAN_STEP1_NFLOG="$ELAN_LOG_DIR/$DATESTAMP/nf.elan.log"
+ELAN_STEP2_NFLOG="$ELAN_LOG_DIR/$DATESTAMP/nf.ocarina.log"
+ELAN_STEP3_LOG="$ELAN_LOG_DIR/$DATESTAMP/publish.log"
+
+ELAN_STEP1_STDOUTERR="$PWD/nf.elan.$DATESTAMP.log"
 
 # OCARINA_FILE only written if elan processed at least one sample
 OCARINA_FILE="$ELAN_DIR/staging/summary/$DATESTAMP/ocarina.files.ls"
@@ -63,25 +62,25 @@ OCARINA_OK_FLAG="$ELAN_DIR/staging/summary/$DATESTAMP/ocarina.ok.flag"
 if [ ! -f "$ELAN_OK_FLAG" ]; then
     # If a log already exists, then the pipeline needs to be resumed
     RESUME_FLAG=""
-    if [ -f "nf.elan.$DATESTAMP.log" ]; then
+    if [ -f "$ELAN_STEP1_STDOUTERR" ]; then
         RESUME_FLAG="-resume"
-        MSG='{"text":"*COG-UK inbound pipeline* Using -resume to re-raise Elan without trashing everything. Delete today'\''s log to force a full restart."}'
-        curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+	MSG='{"text":"*COG-UK inbound pipeline* Using -resume to re-raise Elan without trashing everything. Delete today'\''s log ('$ELAN_STEP1_STDOUTERR') to force a full restart."}'
+        curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
     fi
-    /usr/bin/flock -w 1 /dev/shm/.sam_elan -c "$NEXTFLOW_BIN -log $ELAN_STEP1_NFLOG run elan.nf -c $ELAN_CONFIG --publish $ELAN_DIR --cog_publish $COG_PUBLISHED_DIR --schemegit /cephfs/covid/software/sam/artic-ncov2019 --datestamp $DATESTAMP $RESUME_FLAG > nf.elan.$DATESTAMP.log 2>&1;"
+    /usr/bin/flock -w 1 /dev/shm/.sam_elan -c "$NEXTFLOW_BIN -log $ELAN_STEP1_NFLOG run elan.nf -c $ELAN_CONFIG --publish $ELAN_DIR --cog_publish $COG_PUBLISHED_DIR --schemegit /cephfs/covid/software/sam/artic-ncov2019 --datestamp $DATESTAMP $RESUME_FLAG > $ELAN_STEP1_STDOUTERR 2>&1;"
     ret=$?
 
     if [ $ret -ne 0 ]; then
-        lines=`tail -n 25 nf.elan.$DATESTAMP.log`
+        lines=`tail -n 25 $ELAN_STEP1_STDOUTERR`
     else
-        lines=`awk -vRS= 'END{print}' nf.elan.$DATESTAMP.log`
+        lines=`awk -vRS= 'END{print}' $ELAN_STEP1_STDOUTERR`
     fi
 
     MSG='{"text":"*COG-UK inbound pipeline finished...*
 ...with exit status '"$ret"'
 '"\`\`\`${lines}\`\`\`"'
 _Have a nice day!_"}'
-    curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+    curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
 
     if [ $ret -ne 0 ]; then
         $ELAN_SOFTWARE_DIR/bin/control/handle-elan.sh $DATESTAMP
@@ -91,12 +90,14 @@ _Have a nice day!_"}'
     fi
 else
     MSG='{"text":"*COG-UK inbound pipeline* Cowardly skipping Elan as the OK flag already exists for today"}'
-    curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+    curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
 fi
+
+exit 1
 
 # If the OCARINA_FILE has still not been written at this point, it means Elan ran but today's pipeline is empty - abort early but successfully, dont send a tael
 if [ ! -f "$OCARINA_FILE" ]; then
-    curl -X POST -H 'Content-type: application/json' --data '{"text":"\n*COG-UK inbound pipeline empty*\nNo new valid files today, try again tomorrow."}' $SLACK_REAL_HOOK
+    curl -X POST -H 'Content-type: application/json' --data '{"text":"\n*COG-UK inbound pipeline empty*\nNo new valid files today, try again tomorrow."}' $ELAN_SLACK_INBOUND_HOOK
     exit 0
 fi
 
@@ -108,18 +109,18 @@ if [ ! -f "$OCARINA_OK_FLAG" ]; then
 ...with exit status '"$ret"'
 '"\`\`\`${lines}\`\`\`"'"
 }'
-    curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+    curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
 
     if [ $ret -ne 0 ]; then
         MSG='{"text":"<!channel> *COG-UK inbound pipeline failed (Ocarina)*"}'
-        curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+        curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
         exit $ret
     else
         touch $OCARINA_OK_FLAG
     fi
 else
     MSG='{"text":"*COG-UK inbound pipeline* Cowardly skipping Ocarina as the OK flag already exists for today"}'
-    curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+    curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
 fi
 
 SECONDS=0
@@ -128,10 +129,10 @@ ret=$?
 TIMER=$(python -c "import datetime; print('(publish)', str(datetime.timedelta(seconds=$SECONDS)))")
 MSG='{"text":"*COG-UK publishing pipeline finished...*
 ...with exit status '"$ret"' in '"$TIMER"'"}'
-curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
 if [ $ret -ne 0 ]; then
     MSG='{"text":"<!channel> *COG-UK inbound pipeline failed...*"}'
-    curl -X POST -H 'Content-type: application/json' --data "$MSG" $SLACK_MGMT_HOOK
+    curl -X POST -H 'Content-type: application/json' --data "$MSG" $ELAN_SLACK_MGMT_HOOK
     exit $ret
 fi
 
